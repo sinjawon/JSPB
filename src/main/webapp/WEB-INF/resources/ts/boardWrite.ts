4cbfdb27dc707035bc29efb0791d313ffc30bc2f
jisu <jisu787@gmail.com>
2023-12-29 14:58:32 +0900

    이지수 게시글 jsp, typescript
    임시로 json 받아오기용 api 작성함
    DB와 연결은 아직 안됐고,
     json 만 텍스트로 저장하게 설정됨

---
+++ src/main/webapp/WEB-INF/resources/ts/boardWrite.ts
@@ -0,0 +1,48 @@
+
+//  변경점
+
+class Post {
+    constructor(public title: string, public content: string) {}
+}
+
+
+//확인버튼 동작
+function submitPost() {
+    let titleInput = document.getElementById('titleInput') as HTMLInputElement;
+    let contentTextarea = document.getElementById('contentTextarea') as HTMLTextAreaElement;
+
+    let title = titleInput.value;
+    let content = contentTextarea.value;
+
+    let newPost = new Post(title, content);
+    console.log("저장되었습니다")
+    // JSON 만들어서 보냄
+    ajax('/api/savePostData', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(newPost)
+    }).then((response) => {
+        console.log('글이 성공적으로 저장되었습니다:', response);
+        
+    }).catch((error) => {
+        console.error('데이터 저장 중 오류가 발생했습니다:', error);
+        
+    });
+}
+
+function cancelPost() {
+    // 작성된 내용 초기화
+    let titleInput = document.getElementById('titleInput') as HTMLInputElement;
+    let contentTextarea = document.getElementById('contentTextarea') as HTMLTextAreaElement;
+    
+    titleInput.value = '';
+    contentTextarea.value = '';
+}
+
+
+//임시 ajax
+async function ajax(url: string, option?: any) {
+    return fetch(url, option).then((res) => res.json());
+}
