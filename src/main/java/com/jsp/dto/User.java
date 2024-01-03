821bebcb134238b65ee990e6b7193707b1d2697b
sinjawon <qlqhroal@naver.com>
2024-01-04 04:03:13 +0900

    11
    할로

--- src/main/java/com/jsp/dto/User.java
+++ src/main/java/com/jsp/dto/User.java
@@ -1,6 +1,6 @@
 package com.jsp.dto;
 
-public class UserInfo {
+public class User{
     private int userNum; //Primary Key 가입순서대로 1씩 증가
     private String userId; 
     private String userNickname; // 게시글 작성자 아이디 안보여주려고 가입때 닉네임 설정
